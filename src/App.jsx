49bd323597eb05f93f0fff6f53da1d1755073634
closetgeekshow <closetgeekshow@gmail.com>
2024-12-29 22:43:28 +0000

    css.gui working demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CssGuiDemo from './components/CssGuiDemo/CssGuiDemo'
+import { CssGuiDemo } from './components/CssGuiDemo/CssGuiDemo'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssGuiDemo />
+      <CssGuiDemo />
   </React.StrictMode>
 )
--- src/components/CssGuiDemo/CssGuiDemo.jsx
+++ src/components/CssGuiDemo/CssGuiDemo.jsx
@@ -1,30 +1,36 @@
 import { useState } from 'react'
-import { Editor, styled } from '@compai/css-gui'
+import { Editor, styled, codegen, Inputs } from '@compai/css-gui'
+import './CssGuiDemo.css'
 
-export default function CssGuiDemo() {
+export const CssGuiDemo = () => {
   const [styles, setStyles] = useState({
-    backgroundColor: '#e0e0e0',
-    padding: {
-      value: 20,
-      unit: 'px'
-    },
-    borderRadius: {
-      value: 8,
-      unit: 'px'  
-    },
-    fontSize: {
-      value: 16,
-      unit: 'px'
-    }
+    fontSize: { value: 16, unit: 'px' },
+    lineHeight: { value: 1.4, unit: 'number' },
+    color: 'tomato',
   })
 
   return (
-    <div>
-      <Editor styles={styles} onChange={setStyles} />
-      <styled.div styles={styles}>
-        <h1>CSS GUI Demo</h1>
-        <p>Try editing the styles using the controls above!</p>
-      </styled.div>
-    </div>
+    <>
+      <Editor styles={styles} onChange={setStyles}>
+        <div>
+          <h3>Typography</h3>
+          <Inputs.FontSize />
+          <Inputs.LineHeight />
+          <Inputs.TextAlign />
+          <Inputs.FontStretch />
+          <Inputs.Margin />
+          <Inputs.FontFamily />
+          <h3>Colors</h3>
+          <Inputs.Color />
+          <Inputs.BackgroundColor />
+          <h3>Size</h3>
+          <Inputs.Width />
+          <Inputs.MaxWidth />
+          <Inputs.Height />
+        </div>
+      </Editor>
+      <styled.p styles={styles}>Hello, world!</styled.p>
+      <textarea value={codegen.css(styles)} readOnly></textarea>
+    </>
   )
 }
